c61dd0a023e0ba8a6cc158884ff795d6e1a5fad3
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-10-11 14:30:51 +0300

    Initial Commit

---
+++ iTunnesExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iTunnesExample
+//
+//  Created by İbrahim Erdogan on 11.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTunnesExample/iTunnesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunnesExampleApp.swift
+//  iTunnesExample
+//
+//  Created by İbrahim Erdogan on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct iTunnesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
